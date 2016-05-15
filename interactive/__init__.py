2ab794136c6b12bcfdeecad9ac583cbeb134bb9b
diorge <diorge.bs@gmail.com>
2016-05-15 08:39:44 -0300

    Module description

---
+++ interactive/__init__.py
@@ -0,0 +1,7 @@
+"""
+Interactive Sort
+================
+Sorts a collection of objects without native ordering, by asking someone
+the order of some elements and applying transitivity to find out the total
+ordering for the set.
+"""
