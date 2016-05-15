fc9e4d19d4c6579d534506ff2a4f3850c3211ee8
diorge <diorge.bs@gmail.com>
2016-05-15 10:40:43 -0300

    Additional tests for testing input validation and error handling

--- interactive/test/test_transitivitytable.py
+++ interactive/test/test_transitivitytable.py
@@ -40,3 +40,43 @@ class TestTransitivityTable(TestCase):
         table.order('a', 'b', isort.Ordering.Lower)
         table.order('b', 'c', isort.Ordering.Lower)
         assert table.ishigher('c', 'a')
+
+    def test_construction_invalid_iterator(self):
+        """ Tests the construction when passing an invalid iterator """
+        with self.assertRaises(TypeError) as e:
+            table = isort.TransitivityTable(10)
+
+    def test_construction_empty_iterator(self):
+        """ Tests the construction when passing an empty iterator """
+        table = isort.TransitivityTable([])
+        assert 0 == table.dimension
+
+    def test_orderof_not_existant_origin(self):
+        """ Tests the orderof method when origin is not in the set """
+        table = isort.TransitivityTable(self.dataset)
+        with self.assertRaises(KeyError) as e:
+            s = table.orderof('z', 'a')
+
+    def test_orderof_not_existant_target(self):
+        """ Tests the orderof method when target is not in the set """
+        table = isort.TransitivityTable(self.dataset)
+        with self.assertRaises(KeyError) as e:
+            s = table.orderof('a', 'z')
+
+    def test_order_not_existant_origin(self):
+        """ Tests the order method when giving an invalid origin """
+        table = isort.TransitivityTable(self.dataset)
+        with self.assertRaises(ValueError) as e:
+            table.order('z', 'a', isort.Ordering.Higher)
+
+    def test_order_not_existant_target(self):
+        """ Tests the order method when giving an invalid target """
+        table = isort.TransitivityTable(self.dataset)
+        with self.assertRaises(ValueError) as e:
+            table.order('a', 5, isort.Ordering.Unknown)
+
+    def test_order_invalid_value(self):
+        """ Tests the order method when value is not an Ordering """
+        table = isort.TransitivityTable(self.dataset)
+        with self.assertRaises(TypeError) as e:
+            table.order('a', 'b', 5)
