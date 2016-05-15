9c7057244dddc02facb72edcd4731e2bc218ecb9
diorge <diorge.bs@gmail.com>
2016-05-15 08:46:37 -0300

    Description for the transivity table

--- interactive/sort.py
+++ interactive/sort.py
@@ -5,3 +5,11 @@ Sorts a collection of objects without native ordering, by asking someone
 the order of some elements and applying transitivity to find out the total
 ordering for the set.
 """
+
+
+class TransitivityTable(object):
+    """
+    Transitivity table stores the order between every member of a set.
+    It can be operated to fill vacant slots according to the transivity rule.
+    """
+    pass
